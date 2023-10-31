6cc8f69ffb675614d8f094dfdc53ec1c31af1365
hajarerrozma <hajarerrozma14@gmail.com>
2023-10-31 04:38:50 -0700

    jiji

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,26 +3,17 @@
 
 char *create_array(unsigned int size, char c);
 {
+	char *buffer;
+	unsigned int size;
 
-	char *str;
-
-	unsigned int i;
-
-
-
-	str = malloc(sizeof(char) * size);
-
-	if (size == 0 || str == NULL)
-
-		return (NULL);
-
-
-
-	for (i = 0; i < size; i++)
-
-		str[i] = c;
-
-	return (str);
-
+	size = 72;
+	buffer = create_array(size, 'S');
+	if (buffer == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	simple_print_buffer(buffer, size);
+	free(buffer);
+	return (0);
 }
-
